e1320d4707ad28e08af0bdfb225d33b28061816e
Arseniy <senya.gov@gmail.com>
2024-02-29 14:25:02 +0200

    refactor: rewrite main menu also added account button

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainMenuComponent } from './component/main-menu/main-menu.component';
 import { ClientComponent } from './component/client/client.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainMenuComponent,
-    ClientComponent
+    ClientComponent,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
